764e9458888c8e97b40da796388b98792de4a3dd
MattiX <MattiX@MattiX-PC.mtech>
2011-02-02 17:04:34 -0500

    Added headers and refined api calls

--- src/android/nimpres/client/settings/NimpresSettings.java
+++ src/android/nimpres/client/settings/NimpresSettings.java
@@ -1,3 +1,29 @@
+/**
+ * Project:			Nimpres Android Client
+ * File name: 		NimpresSettings.java
+ * Date modified:	2011-02-02
+ * Description:		Static settings shared throughout app
+ * 
+ * License:			Copyright (c) 2011 (Matthew Brooks, Jordan Emmons, William Kong)
+					
+					Permission is hereby granted, free of charge, to any person obtaining a copy
+					of this software and associated documentation files (the "Software"), to deal
+					in the Software without restriction, including without limitation the rights
+					to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+					copies of the Software, and to permit persons to whom the Software is
+					furnished to do so, subject to the following conditions:
+					
+					The above copyright notice and this permission notice shall be included in
+					all copies or substantial portions of the Software.
+					
+					THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+					IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+					FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+					AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+					LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+					OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+					THE SOFTWARE.
+ */
 package android.nimpres.client.settings;
 
 public class NimpresSettings {
@@ -6,8 +32,20 @@ public class NimpresSettings {
 	 * API Addresses
 	 */
 	
-	public static final String NimpresAPIAddress = "http://droidshare.testing.mattixtech.net/api/";
-	public static final String API_Login_Address = "login.php";
+	public static final String API_BASE_URL = "http://droidshare.testing.mattixtech.net/api/";
+	public static final String API_LOGIN = "login";
+	public static final String API_CREATE_ACCOUNT = "create_account";
+	public static final String API_CREATE_PRESENTATION = "create_presentation";
+	public static final String API_UPDATE_PRESENTATION = "update_presentation";
+	public static final String API_DELETE_PRESENTATION = "delete_presentation";
+	public static final String API_LIST_PRESENTATIONs = "list_presentations";
+	
+	/*
+	 * API Message
+	 */
+	
+	public static final String API_RESPONSE_POSITIVE = "OK";
+	public static final String API_RESPONSE_NEGATIVE = "FAIL";
 	
 	/*
      * Timers:
--- src/android/nimpres/client/web/APIContact.java
+++ src/android/nimpres/client/web/APIContact.java
@@ -1,3 +1,29 @@
+/**
+ * Project:			Nimpres Android Client
+ * File name: 		APIContact.java
+ * Date modified:	2011-02-02
+ * Description:		Static methods for performing api calls to the webserver
+ * 
+ * License:			Copyright (c) 2011 (Matthew Brooks, Jordan Emmons, William Kong)
+					
+					Permission is hereby granted, free of charge, to any person obtaining a copy
+					of this software and associated documentation files (the "Software"), to deal
+					in the Software without restriction, including without limitation the rights
+					to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+					copies of the Software, and to permit persons to whom the Software is
+					furnished to do so, subject to the following conditions:
+					
+					The above copyright notice and this permission notice shall be included in
+					all copies or substantial portions of the Software.
+					
+					THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+					IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+					FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+					AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+					LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+					OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+					THE SOFTWARE.
+ */
 package android.nimpres.client.web;
 
 import java.util.ArrayList;
@@ -17,32 +43,71 @@ import org.apache.http.util.EntityUtils;
 import android.nimpres.client.settings.NimpresSettings;
 import android.util.Log;
 
+/**
+ * 
+ * @author
+ *
+ */
 public class APIContact {
 
-	public static boolean validateLogin(String login, String password){
+	/**
+	 * Returns the full URL of the requested api call
+	 * @param apiCall
+	 * @return the full URL of the requested api call
+	 */
+	public static String getAPIAddress(String apiCall){
+		return NimpresSettings.API_BASE_URL + apiCall +".php";
+	}
+	
+	/**
+	 * Performs a post request to the specified api with the specified parameters and returns the result
+	 * @param apiAddress
+	 * @param postParams
+	 * @return the result in an HttpEntity object
+	 */
+	public static HttpEntity apiPostRequest(String apiAddress,List<NameValuePair> postParams){
+		HttpEntity resEntity = null;
 		try {
-	        HttpClient client = new DefaultHttpClient();  
-	        String postURL = NimpresSettings.NimpresAPIAddress + NimpresSettings.API_Login_Address;
-	        HttpPost post = new HttpPost(postURL); 
-	            List<NameValuePair> params = new ArrayList<NameValuePair>();
-	            params.add(new BasicNameValuePair("login", login));
-	            params.add(new BasicNameValuePair("password", password));
-	            UrlEncodedFormEntity ent = new UrlEncodedFormEntity(params,HTTP.UTF_8);
-	            post.setEntity(ent);
-	            HttpResponse responsePOST = client.execute(post);  
-	            HttpEntity resEntity = responsePOST.getEntity();  
-	            if (resEntity != null) {    
-	                Log.d("APIContact"," API Response: "+EntityUtils.toString(resEntity));
-	              //Check here for valid response
-	                if(EntityUtils.toString(resEntity).equals("OK"))
-	                	return true;
-	                
-	            }
-	    } catch (Exception e) {
+			HttpClient client = new DefaultHttpClient();  
+	        //Set address for API call
+	        HttpPost post = new HttpPost(getAPIAddress(apiAddress));
+	        //Add post parameters to request
+	        UrlEncodedFormEntity ent = new UrlEncodedFormEntity(postParams,HTTP.UTF_8);
+	        post.setEntity(ent);
+	        //Send post and store result
+            HttpResponse responsePOST = client.execute(post);  
+            resEntity = responsePOST.getEntity();
+            if (resEntity != null) { 
+            	return resEntity;
+            }
+		} catch (Exception e) {
 	        e.printStackTrace();
 	    }
+		return null;
+	}
+	
+	/**
+	 * This method validates a login/password combination
+	 * @param login
+	 * @param password
+	 * @return true if valid login, false otherwise
+	 */
+	public static boolean validateLogin(String login, String password){
+		String result = "";
+		List<NameValuePair> params = new ArrayList<NameValuePair>();
+		params.add(new BasicNameValuePair("login", login));
+        params.add(new BasicNameValuePair("password", password));
+        HttpEntity resEntity = apiPostRequest(getAPIAddress(NimpresSettings.API_LOGIN),params);
+		try{
+			result = EntityUtils.toString(resEntity);
+		}catch (Exception e) {
+	        e.printStackTrace();
+	    }
+		if(result.equals(NimpresSettings.API_RESPONSE_POSITIVE))
+			return true;
 		return false;
 	}
+	
 	public static void pushDPSToWeb(String login, String password, String fileName, String pesentationTitle, boolean passwordProtect){
 		
 		/*Posting file cod
