2f14f247c3ff81c06f058586c8e797a37074a542
Matthew Brooks <Matt@mattbrooks.ca>
2011-02-01 21:11:55 -0500

    Worked on Presetation class

--- src/android/nimpres/client/presentation/Presentation.java
+++ src/android/nimpres/client/presentation/Presentation.java
@@ -4,10 +4,10 @@ public class Presentation {
 	private String presentationName = "";
 	private int numSlides = 0;
 	private int currentSlide = 0;
+	private String[] slideFiles;
 	
 	public String getPresentationName() {
-		// TODO Auto-generated method stub
-		return null;
+		return presentationName;
 	}
 	
 	public void setPresentationName(String name){
@@ -15,8 +15,25 @@ public class Presentation {
 	}
 
 	public int getCurrentSlide() {
-		// TODO Auto-generated method stub
-		return 0;
+		return currentSlide;
+	}
+	
+	public void setCurrentSlide(int slideNum){
+		currentSlide = slideNum;
+	}
+	
+	public String getCurrentSlideFile(){
+		return slideFiles[currentSlide];
+	}
+	
+	public void nextSlide(){
+		if(currentSlide < (numSlides-1))
+			currentSlide++;
+	}
+	
+	public void previousSlide(){
+		if(currentSlide>0)
+			currentSlide--;
 	}
 
 }
