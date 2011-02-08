56bf9f47edba00f62027a845d4c37ffbbf9ad28a
Will <Will@172.17.128.204>
2011-02-08 14:59:58 -0500

    Added end presentation screen

--- gen/android/nimpres/R.java
+++ gen/android/nimpres/R.java
@@ -14,30 +14,31 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050009;
-        public static final int button2=0x7f05000a;
+        public static final int button1=0x7f050006;
+        public static final int button2=0x7f050007;
         public static final int button3=0x7f050005;
         public static final int button4=0x7f050004;
         public static final int button5=0x7f05000c;
         public static final int editText1=0x7f050001;
         public static final int editText2=0x7f050003;
-        public static final int editText3=0x7f050007;
+        public static final int editText3=0x7f05000a;
         public static final int imageView1=0x7f05000d;
-        public static final int listView1=0x7f05000b;
+        public static final int listView1=0x7f050008;
         public static final int textView1=0x7f050000;
         public static final int textView2=0x7f050002;
-        public static final int textView3=0x7f050006;
-        public static final int textView4=0x7f050008;
+        public static final int textView3=0x7f050009;
+        public static final int textView4=0x7f05000b;
     }
     public static final class layout {
         public static final int create_account=0x7f030000;
-        public static final int existing_account=0x7f030001;
-        public static final int host_presentation=0x7f030002;
-        public static final int join_presentation=0x7f030003;
-        public static final int main=0x7f030004;
-        public static final int presentation=0x7f030005;
-        public static final int presentation_viewer=0x7f030006;
-        public static final int settings=0x7f030007;
+        public static final int end_presentation=0x7f030001;
+        public static final int existing_account=0x7f030002;
+        public static final int host_presentation=0x7f030003;
+        public static final int join_presentation=0x7f030004;
+        public static final int main=0x7f030005;
+        public static final int presentation=0x7f030006;
+        public static final int presentation_viewer=0x7f030007;
+        public static final int settings=0x7f030008;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
