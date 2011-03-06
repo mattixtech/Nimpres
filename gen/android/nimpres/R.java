da59dfaae87ebb434d10c24b11df35f7c2a15ad8
Will <Will@Will-LT7.mtech>
2011-03-06 14:03:55 -0500

    Changed presentation view layout

--- gen/android/nimpres/R.java
+++ gen/android/nimpres/R.java
@@ -14,19 +14,19 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f050006;
-        public static final int button2=0x7f050007;
-        public static final int button3=0x7f050005;
-        public static final int button4=0x7f050004;
+        public static final int button1=0x7f050007;
+        public static final int button2=0x7f050008;
+        public static final int button3=0x7f050003;
+        public static final int button4=0x7f050005;
         public static final int button5=0x7f05000c;
-        public static final int editText1=0x7f050001;
-        public static final int editText2=0x7f050003;
+        public static final int editText1=0x7f050004;
+        public static final int editText2=0x7f050002;
         public static final int editText3=0x7f05000a;
         public static final int imageView1=0x7f05000d;
-        public static final int listView1=0x7f050008;
+        public static final int listView1=0x7f050009;
         public static final int textView1=0x7f050000;
-        public static final int textView2=0x7f050002;
-        public static final int textView3=0x7f050009;
+        public static final int textView2=0x7f050001;
+        public static final int textView3=0x7f050006;
         public static final int textView4=0x7f05000b;
     }
     public static final class layout {
