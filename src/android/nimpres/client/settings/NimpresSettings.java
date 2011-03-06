ac650895db63c177ffa050bfec057d47285a74fa
mattixtech <matt@mattbrooks.ca>
2011-03-06 16:12:02 -0500

    Changed API address for get_slide_num

--- src/android/nimpres/client/settings/NimpresSettings.java
+++ src/android/nimpres/client/settings/NimpresSettings.java
@@ -34,7 +34,7 @@ public class NimpresSettings {
 	
 	public static final String API_BASE_URL = "http://droidshare.testing.mattixtech.net/api/";
 	public static final String API_LOGIN = "login";
-	public static final String API_SLIDE = "get_slide";
+	public static final String API_SLIDE = "get_slide_num";
 	public static final String API_CREATE_ACCOUNT = "create_account";
 	public static final String API_CREATE_PRESENTATION = "create_presentation";
 	public static final String API_UPDATE_PRESENTATION = "update_presentation";
