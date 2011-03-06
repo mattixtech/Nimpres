7cbeace3481f06c1cd9222a3e761247332704ce1
mattixtech <matt@mattbrooks.ca>
2011-03-06 18:01:28 -0500

    Updated timer

--- src/android/nimpres/NimpresClient.java
+++ src/android/nimpres/NimpresClient.java
@@ -64,7 +64,7 @@ public class NimpresClient extends Activity {
 		 * Testing Code Below
 		 */
 		TimerTask task  = new PresentationUpdater();
-		new Timer().scheduleAtFixedRate(task, 100, 200);
+		new Timer().scheduleAtFixedRate(task, 100, 2000);
 
 		// Set to main view
 		setContentView(R.layout.main);
