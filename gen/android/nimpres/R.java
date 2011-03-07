b5f4b397d751f75be223050618ab7db95e552395
Will <Will@Will-LT7.mtech>
2011-03-06 21:46:58 -0500

    something

--- gen/android/nimpres/R.java
+++ gen/android/nimpres/R.java
@@ -14,29 +14,31 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int button1=0x7f060006;
-        public static final int button2=0x7f060007;
+        public static final int button1=0x7f06000b;
+        public static final int button2=0x7f06000c;
         public static final int button3=0x7f060002;
         public static final int button4=0x7f060004;
-        public static final int button5=0x7f06000c;
+        public static final int button5=0x7f06000e;
         public static final int editText1=0x7f060003;
         public static final int editText2=0x7f060001;
-        public static final int editText3=0x7f06000a;
-        public static final int imageView1=0x7f06000d;
-        public static final int listView1=0x7f060008;
-        public static final int pvNotes=0x7f060010;
-        public static final int pvSlide=0x7f060011;
-        public static final int pvSlideTitle=0x7f06000f;
-        public static final int pvTitle=0x7f06000e;
-        public static final int pvmBack=0x7f060012;
-        public static final int pvmLeave=0x7f060014;
-        public static final int pvmNext=0x7f060013;
-        public static final int pvmPause=0x7f060015;
-        public static final int pvmResume=0x7f060016;
+        public static final int editText3=0x7f060009;
+        public static final int epNo=0x7f060007;
+        public static final int epYes=0x7f060006;
+        public static final int imageView1=0x7f06000f;
+        public static final int listView1=0x7f06000d;
+        public static final int pvNotes=0x7f060012;
+        public static final int pvSlide=0x7f060013;
+        public static final int pvSlideTitle=0x7f060011;
+        public static final int pvTitle=0x7f060010;
+        public static final int pvmBack=0x7f060014;
+        public static final int pvmLeave=0x7f060016;
+        public static final int pvmNext=0x7f060015;
+        public static final int pvmPause=0x7f060017;
+        public static final int pvmResume=0x7f060018;
         public static final int textView1=0x7f060000;
-        public static final int textView2=0x7f060009;
+        public static final int textView2=0x7f060008;
         public static final int textView3=0x7f060005;
-        public static final int textView4=0x7f06000b;
+        public static final int textView4=0x7f06000a;
     }
     public static final class layout {
         public static final int create_account=0x7f030000;
