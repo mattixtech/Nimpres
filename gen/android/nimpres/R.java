6669feb08dbd626bea77c081a7b935ecc1a29366
Will <Will@Will-LT7>
2011-03-09 14:51:05 -0500

    presenter view changed

--- gen/android/nimpres/R.java
+++ gen/android/nimpres/R.java
@@ -11,7 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int h72=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int l36=0x7f020002;
+        public static final int m48=0x7f020003;
     }
     public static final class id {
         public static final int button1=0x7f06000b;
@@ -24,18 +27,21 @@ public final class R {
         public static final int editText3=0x7f060009;
         public static final int epNo=0x7f060007;
         public static final int epYes=0x7f060006;
-        public static final int imageView1=0x7f06000f;
+        public static final int imageView1=0x7f060010;
         public static final int listView1=0x7f06000d;
-        public static final int pvNotes=0x7f060012;
-        public static final int pvSlide=0x7f060013;
-        public static final int pvSlideTitle=0x7f060011;
-        public static final int pvTitle=0x7f060010;
-        public static final int pvmBack=0x7f060014;
-        public static final int pvmJump=0x7f060019;
-        public static final int pvmLeave=0x7f060016;
-        public static final int pvmNext=0x7f060015;
-        public static final int pvmPause=0x7f060017;
-        public static final int pvmResume=0x7f060018;
+        public static final int phvTitle=0x7f06000f;
+        public static final int pvNotes=0x7f060013;
+        public static final int pvSlide=0x7f060014;
+        public static final int pvSlideTitle=0x7f060012;
+        public static final int pvTitle=0x7f060011;
+        public static final int pvhmEnd=0x7f06001c;
+        public static final int pvhmStats=0x7f06001b;
+        public static final int pvmBack=0x7f060015;
+        public static final int pvmJump=0x7f06001a;
+        public static final int pvmLeave=0x7f060017;
+        public static final int pvmNext=0x7f060016;
+        public static final int pvmPause=0x7f060018;
+        public static final int pvmResume=0x7f060019;
         public static final int textView1=0x7f060000;
         public static final int textView2=0x7f060008;
         public static final int textView3=0x7f060005;
@@ -54,6 +60,7 @@ public final class R {
     }
     public static final class menu {
         public static final int pv_menu=0x7f050000;
+        public static final int pvh_menu=0x7f050001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
