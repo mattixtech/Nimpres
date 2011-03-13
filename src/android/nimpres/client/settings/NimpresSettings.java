620a46c7f62556b73d8574eb0587f015d48b4623
Matthew Brooks <matt@mattbrooks.ca>
2011-03-13 18:01:56 -0400

    Improved LANAdvertiser

--- src/android/nimpres/client/settings/NimpresSettings.java
+++ src/android/nimpres/client/settings/NimpresSettings.java
@@ -1,7 +1,7 @@
 /**
  * Project:			Nimpres Android Client
  * File name: 		NimpresSettings.java
- * Date modified:	2011-03-12
+ * Date modified:	2011-03-13
  * Description:		Static settings shared throughout app
  * 
  * License:			Copyright (c) 2011 (Matthew Brooks, Jordan Emmons, William Kong)
@@ -53,7 +53,8 @@ public class NimpresSettings {
      */
     public static final double HELLO_TIMER = 1; //Send hello every X seconds
     public static final double DEAD_TIMER = 5; //Wait X seconds before removing peer
-    public static final int API_PULL_DELAY = 1000; //Wait X seconds before performing another pull from the API
+    public static final int API_PULL_DELAY = 1000; //Wait X miliseconds before performing another pull from the API
+    public static final int LAN_ADVERTISE_DELAY = 1000;
 	
 	/*
      * Ports
