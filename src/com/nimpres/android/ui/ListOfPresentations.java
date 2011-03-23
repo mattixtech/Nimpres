dd76f171180c0d2396ccadcb5ba4e765a3449b88
Will <Will@172.17.68.1>
2011-03-23 16:16:34 -0400

    working on loading presentation list

--- src/com/nimpres/android/ui/ListOfPresentations.java
+++ src/com/nimpres/android/ui/ListOfPresentations.java
@@ -13,12 +13,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.nimpres.R;
 import com.nimpres.android.NimpresObjects;
 
 public class ListOfPresentations extends ListActivity {
-	private ArrayList<String> item = new ArrayList<String>();
+	private ArrayList<Object> item = new ArrayList<Object>();
 
 	/** Called when the activity is first created. */
 	@Override
@@ -35,13 +36,21 @@ public class ListOfPresentations extends ListActivity {
 				String presenterIDString = presenterID.getText().toString();
 				for (int i = 0; i < NimpresObjects.peerPresentations.size(); i++) {
 					if (presenterIDString.equals(NimpresObjects.peerPresentations.get(i).getPresenterName())) {
-						item.add(NimpresObjects.peerPresentations.get(i).getPresentationName());
+						item.add(NimpresObjects.peerPresentations.get(i).getPresentationID());
 						Log.d("ListOfPresentations","added item to list"+ NimpresObjects.peerPresentations.get(i).getPresentationName());
 					}
 				}
-		    	ArrayAdapter<String> presentationList =
-		    		new ArrayAdapter<String>(view.getContext(), R.layout.row, item);
-		    	setListAdapter(presentationList);				
+		    	ArrayAdapter<Object> presentationList =
+		    		new ArrayAdapter<Object>(view.getContext(), R.layout.row, item);
+		    	setListAdapter(presentationList);
+		    	if (item.size() == 0) {
+		    		TextView notice = (TextView) findViewById(R.id.lopEmpty);
+		    		notice.setText("No Presentations are currently behing hosted by that presenter");
+		    	}
+		    	else {
+		    		TextView notice = (TextView) findViewById(R.id.lopEmpty);
+		    		notice.setText("");
+		    	}
 			}
 		});
 	}
@@ -49,11 +58,11 @@ public class ListOfPresentations extends ListActivity {
 	@Override
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 
-		String presentationName = item.get(position);
+		Object presentationID = item.get(position);
 
 		new AlertDialog.Builder(this)
 		.setIcon(R.drawable.icon)
-		.setTitle("[" + presentationName + "]")
+		.setTitle("[" + presentationID + "]")
 		.setPositiveButton("OK", 
 				new DialogInterface.OnClickListener() {
 					
@@ -61,6 +70,7 @@ public class ListOfPresentations extends ListActivity {
 					public void onClick(DialogInterface dialog, int which) {
 						// TODO Auto-generated method stub
 						//TODO send info back to join presentation class
+						
 					}
 				}).show();
 	}
